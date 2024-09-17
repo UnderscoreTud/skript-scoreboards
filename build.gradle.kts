ac29303b392667290dbb8fa0fcc36fb2def898b1
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-09-17 16:18:46 +0300

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.0"
+}
+
+group = "me.tud.skriptscoreboards"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://repo.skriptlang.org/releases/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.SkriptLang:Skript:2.9.2")
+    compileOnly("org.eclipse.jdt:org.eclipse.jdt.annotation:2.3.0")
+    implementation("fr.mrmicky:fastboard:2.1.3")
+}
+
+tasks.withType<ShadowJar> {
+    relocate("fr.mrmicky.fastboard", "me.tud.skriptscoreboards.fastboard")
+}
+
+tasks.processResources {
+    expand(mapOf(
+        "name" to project.name,
+        "version" to project.version,
+        "group" to project.group
+    ))
+    filteringCharset = "UTF-8"
+}
---
+++ src/main/java/me/tud/skriptscoreboards/SkriptScoreboards.java
@@ -0,0 +1,52 @@
+package me.tud.skriptscoreboards;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
+import ch.njol.skript.util.Version;
+import com.google.common.base.Preconditions;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkriptScoreboards extends JavaPlugin {
+
+    private static SkriptScoreboards instance;
+    private SkriptAddon addonInstance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        if (!getServer().getPluginManager().isPluginEnabled("Skript")) {
+            getLogger().severe("Skript could not be found. Disabling skript-scoreboards...");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        if (Skript.getVersion().isSmallerThan(new Version(2, 9, 0))) {
+            getLogger().severe("skript-scoreboards requires Skript 2.9.0 or later. Disabling skript-scoreboards...");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        if (!Skript.isAcceptRegistrations()) {
+            getLogger().severe("Skript is not accepting registrations. Disabling skript-scoreboards...");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        try {
+            addonInstance = Skript.registerAddon(this).setLanguageFileDirectory("lang");
+            addonInstance.loadClasses("me.tud.skriptscoreboards.elements");
+        } catch (Exception e) {
+            getLogger().severe("An error occurred while loading skript-scoreboards. Disabling skript-scoreboards...");
+            getServer().getPluginManager().disablePlugin(this);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SkriptAddon getAddonInstance() {
+        return addonInstance;
+    }
+
+    public static SkriptScoreboards instance() {
+        Preconditions.checkState(instance != null, "Plugin not enabled yet");
+        return instance;
+    }
+
+}
