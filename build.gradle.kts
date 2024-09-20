01b92a61b6c4bb3320313945c04491b80503e662
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-09-21 01:40:30 +0300

    Add a way to locally define the destination and name of the built jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.util.Properties
 
 plugins {
     id("java")
@@ -23,6 +24,19 @@ dependencies {
     compileOnly("com.github.SkriptLang:Skript:2.9.2")
     compileOnly("org.eclipse.jdt:org.eclipse.jdt.annotation:2.3.0")
     implementation("fr.mrmicky:fastboard:2.1.3")
+}
+
+val properties = Properties()
+if (file("local.properties").exists()) {
+    properties.load(file("local.properties").inputStream())
+}
+val jarName: String? = properties.getProperty("jarName")
+val jarDir: String? = properties.getProperty("jarDir")
+
+tasks.withType<Jar> {
+    archiveFileName.set(jarName ?: "${project.name}-${project.version}.jar")
+    if (jarDir != null)
+        destinationDirectory.set(project.file(jarDir))
 }
 
 tasks.withType<ShadowJar> {
