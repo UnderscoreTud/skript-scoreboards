8e3f83aef47d6fde228e37d39267a3463cfaff87
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-09-22 01:31:13 +0300

    Return early in onEnable after disabling the plugin

--- src/main/java/me/tud/skriptscoreboards/SkriptScoreboards.java
+++ src/main/java/me/tud/skriptscoreboards/SkriptScoreboards.java
@@ -4,6 +4,7 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
 import ch.njol.skript.util.Version;
 import com.google.common.base.Preconditions;
+import me.tud.skriptscoreboards.fastboard.FastBoardManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkriptScoreboards extends JavaPlugin {
@@ -18,16 +19,19 @@ public final class SkriptScoreboards extends JavaPlugin {
         if (!getServer().getPluginManager().isPluginEnabled("Skript")) {
             getLogger().severe("Skript could not be found. Disabling skript-scoreboards...");
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
 
         if (Skript.getVersion().isSmallerThan(new Version(2, 9, 0))) {
             getLogger().severe("skript-scoreboards requires Skript 2.9.0 or later. Disabling skript-scoreboards...");
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
 
         if (!Skript.isAcceptRegistrations()) {
             getLogger().severe("Skript is not accepting registrations. Disabling skript-scoreboards...");
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
 
         try {
